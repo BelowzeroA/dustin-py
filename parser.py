3805286313ccb33c01d7e460795f0c9c4e787e46
BelowzeroA <fitty@mail.ru>
2016-11-02 19:11:31 +0300

    update 1

--- parser.py
+++ parser.py
@@ -1,23 +1,31 @@
 import numpy as np
 import json
+import nltk
+import tokenizers as tok
 
-#n = np.ndarray([2])
-#print(n)
+
+# n = np.ndarray([2])
+# print(n)
 
 class Domain:
-
     def load(self, filename):
         f = open(filename, 'r', encoding='utf8')
         self.json = json.load(f)
+
 
 class KnowledgeGraph:
     def __init__(self):
         a = 0
 
+    def parse_query(self, q):
+        self.currentQ = q
+
+
 class QAArray:
     def load(self, filename):
         f = open(filename, 'r', encoding='utf8')
         self.json = json.load(f)
+
 
 dp = Domain()
 dp.load('domain.json')
@@ -25,5 +33,9 @@ dp.load('domain.json')
 qa = QAArray()
 qa.load("qa.json")
 
+kg = KnowledgeGraph()
+
 for x in qa.json:
-    print(x.keys())
+    for key in iter(x.keys()):
+        kg.parse_query(key)
+        print(tok.tokzr_WORD(key))
